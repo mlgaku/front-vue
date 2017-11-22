601780d247eb0684232d7a222e3fa4177bae6175
sxyazi <sxyazi@gmail.com>
2017-11-22 21:34:09 +0800

    modified:   src/components/Avatar.vue

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -9,41 +9,46 @@ export default {
     },
 
     computed: {
+        id () {
+            return `avatar-${this.user.name}-${Math.random()}`
+        },
         ...mapGetters([
             'avatarURL'
         ])
     },
 
     directives: {
-        avatar: {
-            inserted: (el, binding) => {
-                const { url, name, avatar } = binding.value
-
-                if (avatar) {
-                    el.src = url
-                    return
-                }
-
-                fetch(url)
-                .then(res => res.blob())
-                .then(blob => {
-                    const reader = new FileReader()
-                    reader.onload = () => {
-                        const data = reader.result
-                        if (Crc32(data) === 2137162192) {
-                            el.src = Draw(name)
-                        } else {
-                            el.src = `data:image/jpeg;base64,${btoa(data)}`
-                        }
-                    }
-                    reader.readAsBinaryString(blob)
-                })
+        avatar: (el, binding) => {
+            const { id, url, name, avatar } = binding.value
+            if (avatar === true) {
+                el.src = url
+                return
             }
+
+            fetch(url)
+            .then(res => res.blob())
+            .then(blob => {
+                const reader = new FileReader()
+                reader.onload = () => {
+                    const el = document.getElementById(id)
+                    if (!el) {
+                        return
+                    }
+
+                    const data = reader.result
+                    if (Crc32(data) === 2137162192) {
+                        el.src = Draw(name)
+                    } else {
+                        el.src = `data:image/jpeg;base64,${btoa(data)}`
+                    }
+                }
+                reader.readAsBinaryString(blob)
+            })
         }
     }
 }
 </script>
 
 <template>
-    <img v-avatar="{ url: avatarURL(user), name: user.name, avatar: user.avatar }">
+<img :id="id" v-avatar="{ id, url: avatarURL(user), name: user.name, avatar: user.avatar }"/>
 </template>
